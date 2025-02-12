608da77a195fa61d4e37778c92d01ae01604ea34
MJ <michael.muscara@student.fairfield.edu>
2025-02-12 15:15:06 -0500

    tested methods

--- linkedList/LinkedListTester.java
+++ linkedList/LinkedListTester.java
@@ -7,6 +7,14 @@ public class LinkedListTester {
 		
 		LinkedList tester = new LinkedListImpl();
 		tester.addItem("an item to add");
+		tester.addItem("num 2");
+		tester.addItem("num 3");
+
+		System.out.println(tester.itemCount());
+		tester.listItems();
+		System.out.println(tester.isItemInList("numhjh 2"));
+		tester.deleteItem("num 2");
+		tester.listItems();
 		//cool comment
 
 	}
