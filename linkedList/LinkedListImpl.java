b9522d17c65172be7c826dab079479fdf59251d5
MJ <michael.muscara@student.fairfield.edu>
2025-02-23 18:54:07 -0500

    linked list finished

--- linkedList/LinkedListImpl.java
+++ linkedList/LinkedListImpl.java
@@ -63,11 +63,15 @@ public class LinkedListImpl implements LinkedList {
 		// TODO Auto-generated method stub
 
 		ListItem current = head;
-
+		System.out.print("[");
 		while (current != null) {
-			System.out.println(current.data);
+			System.out.print(current.data);
 			current = current.next;
+			if (current != null) {
+				System.out.print(", ");
+			}
 		}
+		System.out.println("]");
 		
 	}
 
@@ -76,6 +80,10 @@ public class LinkedListImpl implements LinkedList {
 
 		if (head == null) {
 			return false;
+		}
+		else if (head.data.equals(thisItem)) {
+			head = head.next;
+			return true;
 		}
 		else {
 			ListItem current = head;
@@ -96,19 +104,73 @@ public class LinkedListImpl implements LinkedList {
 	@Override
 	public Boolean insertBefore(String newItem, String itemToInsertBefore) {
 		// TODO Auto-generated method stub
-		return null;
+		ListItem current = head;
+
+		while (current != null && current.next != null) {
+			if (current.data.equals(itemToInsertBefore)) {
+				ListItem newNode = new ListItem(newItem);
+				newNode.next = head;
+				head = newNode;
+				return true;
+			}
+			else if (current.next.data.equals(itemToInsertBefore)) {
+
+				ListItem newNode = new ListItem(newItem);
+				newNode.next = current.next;
+				current.next = newNode;
+				return true;
+			}
+			current = current.next;
+		}
+
+		return false;
 	}
 
 	@Override
 	public Boolean insertAfter(String newItem, String itemToInsertAfter) {
 		// TODO Auto-generated method stub
-		return null;
+		ListItem current = head;
+
+
+		while (current != null) {
+			if (current.data.equals(itemToInsertAfter)) {
+
+				ListItem newNode = new ListItem(newItem);
+				newNode.next = current.next;
+				current.next = newNode;
+				return true;
+			}
+			current = current.next;
+		}
+
+		return false;
 	}
 
 	@Override
 	public void sort() {
 		// TODO Auto-generated method stub
-		
+		if (head == null || head.next == null) {
+			return; // If the list is empty or has only one element, no need to sort
+		}
+
+		boolean swapped = true; // To make sure we enter the loop at least once
+
+		while (swapped) {
+			swapped = false;
+			ListItem current = head;
+
+			// Traverse the list
+			while (current != null && current.next != null) {
+				if (current.data.compareTo(current.next.data) > 0) {
+					// Swap data between current and next if they are out of order
+					String temp = current.data;
+					current.data = current.next.data;
+					current.next.data = temp;
+					swapped = true; // A swap was made, so we need another pass
+				}
+				current = current.next; // Move to the next node
+			}
+		}
 	}
 	
 }
