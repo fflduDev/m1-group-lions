69cf8b6f5e5749a59b07524b096b542b7fbccaef
MJ <michael.muscara@student.fairfield.edu>
2025-02-25 18:13:16 -0500

    finished linked list part

--- app/StagBusClient.java
+++ app/StagBusClient.java
@@ -2,6 +2,7 @@ package app;
  
 import linkedList.LinkedList;
 import linkedList.LinkedListImpl;
+import linkedList.LinkedListTester;
 import queue.Queue;
 import queue.QueueImpl;
 import stack.Stack;
@@ -13,9 +14,28 @@ public class StagBusClient {
 		// create implementation, then
 
 		System.out.println("-----L I S T  T E S T------");
-		
+
+		System.out.println();
+		LinkedList list = new LinkedListImpl();
 		//listRunTestMethod...
- 		
+ 		list.addItem("Campus Circle");
+		 list.addItem("Train Station");
+		 list.addItem("Stop N Shop");
+		 list.addItem("Beach");
+		 list.listItems();
+		 System.out.println("Is Train Station in the list: " + list.isItemInList("Train Station"));
+		System.out.println("Is Chipotle in the list: " + list.isItemInList("Chipotle"));
+		list.deleteItem("Beach");
+		System.out.print("Removing Beach: ");
+		list.listItems();
+		list.insertBefore("McDonalds", "Stop N Shop");
+		System.out.print("Inserting McDonalds before Stop N Shop: ");
+		list.listItems();
+		list.insertAfter("CVS", "Train Station");
+		System.out.print("Inserting CVS after Train Station: ");
+		list.listItems();
+		System.out.println();
+
 		System.out.println("-----S T A C K  T E S T------");
 
 		//QRunTestMethod...
